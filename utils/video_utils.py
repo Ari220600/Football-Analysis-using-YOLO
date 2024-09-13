8b0b61627ecfb8d02b4b0381c807f067022fd501
Arindam Chakraborty <114055313+Ari220600@users.noreply.github.com>
2024-09-13 12:39:26 +0530

    Add files via upload

---
+++ utils/video_utils.py
@@ -0,0 +1,24 @@
+import cv2
+
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []  # initalising empty frames
+    while True:
+        ret, frame = cap.read()
+        if not ret:  # if falls video will end
+            break
+        frames.append(frame)
+
+    return frames
+def save_video(output_video_frames, output_video_path):
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  
+    print("first frame")  # shape of the first frame
+    print(output_video_frames[0])
+    print("second frame") # shape of the second frame
+    out = cv2.VideoWriter(output_video_path, fourcc, 24,(output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+
+    for frame in output_video_frames:
+        out.write(frame)
+
+    out.release()
